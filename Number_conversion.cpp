debc65751278e9b45b20e65c713ae1111a3ed4f6
Genboyz <olimarstamirayehu3@gmail.com>
2023-04-08 19:18:37 +0300

    fatal

--- Number_conversion.cpp
+++ Number_conversion.cpp
@@ -1,10 +1,6 @@
-#include <iostream>
-using namespace std; 
-
-int main () {
-
-
-cout << "Hello world"; 
-
-    return 0; 
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"let's try this one";
+    return 0;
 }
